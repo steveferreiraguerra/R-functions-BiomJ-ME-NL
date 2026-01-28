4e31f55d6343c7422ea4807920f117b72b657b48
steveferreiraguerra <39748713+steveferreiraguerra@users.noreply.github.com>
2026-01-28 11:12:04 -0500

    Add files via upload

---
+++ Normal - f3 - S shape.R
@@ -0,0 +1,79 @@
+rm(list = ls())
+
+library(foreach)
+library(doParallel)
+
+cores=detectCores()
+cl <- makeCluster(cores[1]-1) #not to overload your computer
+registerDoParallel(cl)
+
+NL_ME <- function(N, SD_ME, beta_0){
+  
+  out = foreach(i = 1:500) %dopar% {
+    
+    library(splines)
+    library(mgcv)
+    library(mfp)
+    
+    expit <- function(x){exp(x)/(1+exp(x))}
+    
+    seeds = read.table("seeds.txt")
+    
+    n <- N
+    
+    x.pred <- seq(100,200, 100/299)
+    
+    set.seed(seeds[i,1])
+    
+    X <- rnorm(n,150,15)
+    X.star <- X + rnorm(n, 0, SD_ME)
+    mean.X = 150
+    sd.X = 30
+    X.std = (X - mean.X)/sd.X
+    
+    fx_3 <- beta_0 + (-1.4)/(1 + ((X.std+5/3)/1.7)^6)
+    #plot(x=X, y=fx_3, cex = 0.5)
+    
+    p_3 <- expit(fx_3)
+    #mean(p_3)  
+    
+    Y <- rbinom(n, 1, p_3)
+    
+    ### Regression ###
+    
+    reg.bs.X <- glm(Y ~ -1 + bs(X, degree=3, intercept=T, knots = c(median(X))), family = "binomial")
+    reg.bs.X.star <- glm(Y ~ -1 + bs(X.star, degree=3, intercept=T, knots = c(median(X.star))), family = "binomial")
+    
+    fp.X <- mfp(Y ~ fp(X, select = 1, alpha = 1), family = "binomial")
+    fp.X.star <- mfp(Y ~ fp(X.star, select = 1, alpha = 1), family = "binomial")
+    
+    ps.X <- gam(Y ~ s(X, bs="ps", k = 14), family = "binomial", method = "REML")
+    ps.X.star <- gam(Y ~ s(X.star, bs="ps", k = 14), family = "binomial", method = "REML")
+    
+    ns.X <- glm(Y ~ ns(X, knots = c(median(X)), Boundary.knots = quantile(X, probs = c(0.05, 0.95))), family = "binomial")
+    ns.X.star <- glm(Y ~ ns(X.star, knots = c(median(X.star)), Boundary.knots = quantile(X.star, probs = c(0.05, 0.95))), family = "binomial")
+    
+    pred_reg.bs.X <- predict(reg.bs.X, type = "link", newdata = data.frame(X = x.pred))
+    pred_reg.bs.X.star <- predict(reg.bs.X.star, type = "link", newdata = data.frame(X.star = x.pred))
+    
+    pred_reg.FP.X <- predict(fp.X, type = "link", newdata = data.frame(X = x.pred))
+    pred_reg.FP.X.star <- predict(fp.X.star, type = "link", newdata = data.frame(X.star = x.pred))
+    
+    pred_reg.ps.X <- predict(ps.X, type = "link", newdata = data.frame(X = x.pred))
+    pred_reg.ps.X.star <- predict(ps.X.star, type = "link", newdata = data.frame(X.star = x.pred))
+    
+    pred_reg.ns.X <- predict(ns.X, type = "link", newdata = data.frame(X = x.pred))
+    pred_reg.ns.X.star <- predict(ns.X.star, type = "link", newdata = data.frame(X.star = x.pred))
+    
+    rbind(pred_reg.bs.X, pred_reg.bs.X.star, 
+          pred_reg.FP.X, pred_reg.FP.X.star,
+          pred_reg.ps.X, pred_reg.ps.X.star,
+          pred_reg.ns.X, pred_reg.ns.X.star)
+    
+  }
+  
+  return(out)
+  
+}
+
+stopCluster(cl)
---
+++ SBP - f1 - Linear - weak.R
@@ -0,0 +1,83 @@
+rm(list = ls())
+
+library(foreach)
+library(doParallel)
+
+cores=detectCores()
+cl <- makeCluster(cores[1]-1)
+registerDoParallel(cl)
+
+NL_ME <- function(N, SD_ME, beta_0){
+  
+  out = foreach(i = 1:500) %dopar% {
+    
+    library(splines)
+    library(mgcv)
+    library(mfp)
+    
+    expit <- function(x){exp(x)/(1+exp(x))}
+    
+    seeds = read.table("seeds.txt")
+    
+    dat.fhs <- read.table("FHS_trunc.txt")
+    
+    n <- N
+    
+    x.pred <- seq(100,200, 100/299)
+    
+    set.seed(seeds[i,1])
+    
+    dat.sim <- dat.fhs[sample(c(1:nrow(dat.fhs)), n, replace = T),]
+    
+    X <- dat.sim$sbp
+    X.star <- X + rnorm(n, 0, SD_ME)
+    
+    beta_1 <- log(1.2^(1/20))
+    
+    fx_1 <- beta_0 + beta_1*X
+    
+    # plot(x=X, y=fx_1, cex = 0.5)
+    
+    p_1 <- expit(fx_1)
+    #mean(p_1)
+    
+    Y <- rbinom(n, 1, p_1)
+    
+    ### Regression ###
+    
+    reg.bs.X <- glm(Y ~ -1 + bs(X, degree=3, intercept=T, knots = c(median(X))), family = "binomial")
+    reg.bs.X.star <- glm(Y ~ -1 + bs(X.star, degree=3, intercept=T, knots = c(median(X.star))), family = "binomial")
+    
+    fp.X <- mfp(Y ~ fp(X, select = 1, alpha = 1), family = "binomial")
+    fp.X.star <- mfp(Y ~ fp(X.star, select = 1, alpha = 1), family = "binomial")
+    
+    ps.X <- gam(Y ~ s(X, bs="ps", k = 14), family = "binomial", method = "REML")
+    ps.X.star <- gam(Y ~ s(X.star, bs="ps", k = 14), family = "binomial", method = "REML")
+    
+    ns.X <- glm(Y ~ ns(X, knots = c(median(X)), Boundary.knots = quantile(X, probs = c(0.05, 0.95))), family = "binomial")
+    ns.X.star <- glm(Y ~ ns(X.star, knots = c(median(X.star)), Boundary.knots = quantile(X.star, probs = c(0.05, 0.95))), family = "binomial")
+    
+    pred_reg.bs.X <- predict(reg.bs.X, type = "link", newdata = data.frame(X = x.pred))
+    pred_reg.bs.X.star <- predict(reg.bs.X.star, type = "link", newdata = data.frame(X.star = x.pred))
+    
+    pred_reg.FP.X <- predict(fp.X, type = "link", newdata = data.frame(X = x.pred))
+    pred_reg.FP.X.star <- predict(fp.X.star, type = "link", newdata = data.frame(X.star = x.pred))
+    
+    pred_reg.ps.X <- predict(ps.X, type = "link", newdata = data.frame(X = x.pred))
+    pred_reg.ps.X.star <- predict(ps.X.star, type = "link", newdata = data.frame(X.star = x.pred))
+    
+    pred_reg.ns.X <- predict(ns.X, type = "link", newdata = data.frame(X = x.pred))
+    pred_reg.ns.X.star <- predict(ns.X.star, type = "link", newdata = data.frame(X.star = x.pred))
+    
+    rbind(pred_reg.bs.X, pred_reg.bs.X.star, 
+          pred_reg.FP.X, pred_reg.FP.X.star,
+          pred_reg.ps.X, pred_reg.ps.X.star,
+          pred_reg.ns.X, pred_reg.ns.X.star)
+    
+  }
+  
+  return(out)
+  
+}
+
+stopCluster(cl)
